8b639811cea935d811015ed0152641aa0cdecaac
abhi-bansal-70 <128970846+abhi-bansal-70@users.noreply.github.com>
2023-12-10 13:33:29 +0000

    arrrays things

---
+++ basics1/myArray.js
@@ -0,0 +1,17 @@
+const myArray = [1,2,3,4];
+const mynewArray = new Array(1,2,3,4);
+console.log(myArray[1]);
+const dcHeroes = ["batman","superman","flash"];
+console.log(dcHeroes.length);
+// dcHeroes[dcHeroes.length] = "wonder women";
+dcHeroes.push("wonder women");
+console.log(dcHeroes);
+dcHeroes.pop();
+// dcHeroes.pop();
+console.log(dcHeroes);
+console.log(myArray.toString());
+
+dcHeroes.slice(1,3);
+dcHeroes.splice(1,3);
+
+
