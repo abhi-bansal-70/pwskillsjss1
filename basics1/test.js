5924f903e7b9bdfda43031193f896c7dde0b3887
abhi-bansal-70 <128970846+abhi-bansal-70@users.noreply.github.com>
2023-12-10 07:35:10 +0000

    about string

--- basics1/test.js
+++ basics1/test.js
@@ -1,1 +1,7 @@
 console.log("abhishek");
+let email = "h@hc.com";
+console.log(email.trim());
+let emailTwo = "abhi@pw.live";
+console.log(emailTwo.replace('abhi','abhishek'));
+console.log(emailTwo.includes('abhi'));
+console.log(emailTwo.toUpperCase());
