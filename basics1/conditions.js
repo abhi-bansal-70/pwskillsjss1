c916bffabe1d279f532a2df2e5220973b1b0f4ca
abhi-bansal-70 <128970846+abhi-bansal-70@users.noreply.github.com>
2023-12-11 14:21:31 +0000

    short hand notes

--- basics1/conditions.js
+++ basics1/conditions.js
@@ -7,12 +7,32 @@ if (temp < 20){
 console.log("atmoshphere is amazing");
 
 let score = 120;
+let life = 3;
+let bullets = 1000;
 console.log("welcome to the game ");
 if(score>=100){
     console.log("you get a new life for a game");
     score -= 100;
+    life++;
+    let bullets = 2000;
+    console.log(`number of present bullets are ${bullets}`);
 } else {
     console.log("sorry, you dont get a life");
 }
 
-console.log(`your score is ${score}`);
+console.log(`your score is ${score} and my life is ${life} and number of bullets are ${bullets}`);
+
+let getStarfromUser = "7";
+let starInNumber = parseInt(getStarfromUser);
+if(starInNumber == 1){
+    console.log("poor");
+} else if(starInNumber == 2){
+    console.log("umm,ok");
+} else if(starInNumber == 3){
+    console.log("average");
+}else if(starInNumber == 4){
+    console.log("good");
+}
+else {
+    console.log("great");
+}
