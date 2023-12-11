291b904a6bc635b821a15e9a616163a15f93581d
abhi-bansal-70 <128970846+abhi-bansal-70@users.noreply.github.com>
2023-12-11 12:49:14 +0000

    about conditions and game changes about in javascript

---
+++ basics1/conditions.js
@@ -0,0 +1,18 @@
+let temp = 16;
+if (temp < 20){
+    console.log("its really cold");
+} else {
+    console.log("its not really cold");
+}
+console.log("atmoshphere is amazing");
+
+let score = 120;
+console.log("welcome to the game ");
+if(score>=100){
+    console.log("you get a new life for a game");
+    score -= 100;
+} else {
+    console.log("sorry, you dont get a life");
+}
+
+console.log(`your score is ${score}`);
