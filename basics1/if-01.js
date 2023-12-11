88ce33b4b585e1b87ddba8fbec1171ee215ed1b2
abhi-bansal-70 <128970846+abhi-bansal-70@users.noreply.github.com>
2023-12-11 16:27:52 +0000

    and or operation

---
+++ basics1/if-01.js
@@ -0,0 +1,40 @@
+let email = "abhi@google.com";
+// if (email == "abhishek@google.com"){
+//     console.log("please enter your password");
+// } else {
+//     console.log("please enter your email id");
+// }
+if (email != ""){
+    console.log("please enter your password");
+} else {
+    console.log("please enter your email id");
+}
+
+
+console.log(Boolean(email));
+
+const email1 = "ab@google.com";
+const password1 ="1235689";
+if(email1){
+    if(password1){
+        console.log("welcome to the app");
+    }
+    else{
+        console.log("please enter the password");
+    }
+}else{
+    console.log("please enter the your email id");
+}
+if(email && password1){
+    console.log("please welcome to the app");
+}else{
+    console.log("please enter your email and password");
+}
+
+const googleLogInToken ="";
+const githubLogInToken ="";
+if(githubLogInToken || googleLogInToken){
+    console.log("welcome to the app");
+} else{
+    console.log("please view your website");
+}
