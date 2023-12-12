75f8fa816629f998dfdaf1d669347998a5f9fa28
abhi-bansal-70 <128970846+abhi-bansal-70@users.noreply.github.com>
2023-12-12 15:48:18 +0000

    loops

---
+++ basics1/01for.js
@@ -0,0 +1,12 @@
+for(let userScore = 1; userScore <= 10; userScore++){
+    console.log(`userscore is ${userScore}`);
+}
+let userScore = 10;
+let min_value = 0;
+for(userScore; userScore >= min_value; userScore--){
+    if(userScore == 5){
+        continue
+    }
+    console.log(`userscore is ${userScore}`);
+
+}
