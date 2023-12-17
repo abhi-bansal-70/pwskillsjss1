1c9e6e9b5102b5bebfde584d25a0c5088f12cbd3
abhi-bansal-70 <128970846+abhi-bansal-70@users.noreply.github.com>
2023-12-17 04:17:02 +0000

    callback

--- basics1/advanceJs.js
+++ basics1/advanceJs.js
@@ -1,8 +1,8 @@
 function processArray(array,fn){
     for (let i = 0; i < array.length; i++) {
-        console.log("index =>", i, "index value",array[i],"function return the value",fn(array[i]);
+        console.log("index =>",i,"index value =>",array[i],"function return the value =>",fn(array[i]));
         
     }
 
 }
-processArray([1,2,3,4,5,6,7,8,9],function cube(x){return x*x*x;})
+processArray([3,2,6,8,1,4],function cube(x){return x*x*x;})
