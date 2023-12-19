4bcc144ea546b4e138a60a36636c56b974559778
abhi-bansal-70 <128970846+abhi-bansal-70@users.noreply.github.com>
2023-12-19 05:18:56 +0000

    fun of scopes

---
+++ basics1/scope.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+var teacher = "abhishek bansal";
+function fun(){
+    var teacher = "rahul";
+    contents = "js";
+    console.log("hello",teacher)
+}
+function gun(){
+    var student = "virat";
+    console.log("welcome to the class", student);
+}
+
+// console.log(student);
+fun();
+gun();
+console.log(teacher,contents);
+
