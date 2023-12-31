c4f20f046396191e35d965a7f5c03d4a7811d147
abhi-bansal-70 <128970846+abhi-bansal-70@users.noreply.github.com>
2023-12-31 06:10:27 +0000

    pattern

--- basics1/loops.js
+++ basics1/loops.js
@@ -1,16 +1,7 @@
-let n = 50; // number of rows
-let string = ""; // empty string to store the pattern
-
-// outer loop for the rows
-for (let i = 1; i <= n; i++) {
-  // inner loop for the columns
-  for (let j = 1; j <= i; j++) {
-    // append a star to the string
-    string += "*";
-  }
-  // append a newline to the string
-  string += "\n";
+let n=50;
+for(let i=1; i<=n; i++){
+    for(let j=1; j<=i; j++){
+        process.stdout.write("*")
+    }
+    console.log();
 }
-
-// print the string
-console.log(string);
